7b4c27450e388ab2583ad2e0dfe9d0e8a87f403b
Peter Sampson <petersampson010@gmail.com>
2019-06-07 09:41:15 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -195,7 +195,7 @@ describe "#display_board in 'lib/display_board.rb" do
       
       expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" O | O | o ")
+      expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
       expect(rows[4]).to eq(" O | O | O ")
       
